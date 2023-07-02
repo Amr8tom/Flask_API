a80a9f28ede6cd81259cc9bd0e730ec000435663
Amr alaa ali <92322929+Amr8tom@users.noreply.github.com>
2023-07-02 14:50:02 +0300

    yarab

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #from flask import Flask , request , render_template ,url_for,jsonify
 import cv2
-# import base64
+import base64
 #from PIL import Image
 # import io
 import numpy as np
@@ -11,31 +11,30 @@ from keras import load_model
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=["POST","GET"])
 def index():
-    return jsonify({'prediction': "cancer"})
-    #if request.method=='POST':
-    #     image_file = request.files['image']
-    #     image_bytes = image_file.read()
-    #     image_np = np.frombuffer(image_bytes, np.uint8)
-    #     image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
-    #     # Check if the image was loaded successfully
-    #     if image is None:
-    #         return 'Failed to load image', 400
+    if request.method=='POST':
+        image_file = request.files['image']
+        image_bytes = image_file.read()
+        image_np = np.frombuffer(image_bytes, np.uint8)
+        image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+        # Check if the image was loaded successfully
+        if image is None:
+            return 'Failed to load image', 400
         
-    #     # Resize the image using cv2.resize()
-    #     resized_image = cv2.resize(image, (50, 50))
-    #     normalized_image = resized_image / 255.0
-    #     input_image = np.expand_dims(normalized_image, axis=0)
-    #     assert input_image.shape == (1, 50, 50, 3)
-    #     # y = my_model.predict([input_image])
-        # y=[0.4,3.5]
-        #y=y.tolist()
-        # ind= np.argmax(y)
-        # y=classes[ind]
-    #     return jsonify({'prediction': "cancer"})
-    #  else:
-    # return "ya raaaaaaab"
+        # Resize the image using cv2.resize()
+        resized_image = cv2.resize(image, (50, 50))
+        normalized_image = resized_image / 255.0
+        input_image = np.expand_dims(normalized_image, axis=0)
+        assert input_image.shape == (1, 50, 50, 3)
+        # y = my_model.predict([input_image])
+        y=[0.4,3.5]
+        y=y.tolist()
+        ind= np.argmax(y)
+        y=classes[ind]
+        return jsonify({'prediction': "cancer"})
+    else:
+        return jsonify({'prediction': "ya raaaaaaaab"})
 
 
 
