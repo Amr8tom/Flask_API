72ecae5b65cda03414b37c499667391e052354f8
Amr alaa ali <92322929+Amr8tom@users.noreply.github.com>
2023-07-03 00:23:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
-# from tensorflow import keras
-# from keras import models
+from tensorflow import keras
+from keras import models
 
 
 
@@ -15,34 +15,32 @@ app = Flask(__name__)
 def home ():
     return jsonify({'prediction': "ya raaaaaaaab"})
 
-
 classes = ["non-cancer","cancer"]
-# my_model= models.load_model("model87.h5")
+my_model= models.load_model("model87.h5")
 
-# @app.route('/pred',methods=["POST","GET"])
-# def index():
-#     if request.method=='POST':
-#         image_file = request.files['image']
-#         image_bytes = image_file.read()
-#         image_np = np.frombuffer(image_bytes, np.uint8)
-#         image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
-#         # Check if the image was loaded successfully
-#         if image is None:
-#             return 'Failed to load image', 400
+@app.route('/predict',methods=["POST"])
+def index():
+    if request.method=='POST':
+        image_file = request.files['image']
+        image_bytes = image_file.read()
+        image_np = np.frombuffer(image_bytes, np.uint8)
+        image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+        # Check if the image was loaded successfully
+        if image is None:
+            return 'Failed to load image', 400
         
-#         # Resize the image using cv2.resize()
-#         resized_image = cv2.resize(image, (50, 50))
-#         normalized_image = resized_image / 255.0
-#         input_image = np.expand_dims(normalized_image, axis=0)
-#         assert input_image.shape == (1, 50, 50, 3)
-#         # y = my_model.predict([input_image])
-#         # y=y.tolist()
-#         y=[0.3,0.5]
-#         ind= np.argmax(y)
-#         y=classes[ind]
-#         return jsonify({'prediction':y})
-#     else:
-#         return jsonify({'prediction': "ya raaaaaaaab"})
+        # Resize the image using cv2.resize()
+        resized_image = cv2.resize(image, (50, 50))
+        normalized_image = resized_image / 255.0
+        input_image = np.expand_dims(normalized_image, axis=0)
+        assert input_image.shape == (1, 50, 50, 3)
+        y = my_model.predict([input_image])
+        y=y.tolist()
+        ind= np.argmax(y)
+        y=classes[ind]
+        return jsonify({'prediction':y})
+    else:
+        return jsonify({'prediction': "ya raaaaaaaab"})
 
 
 
