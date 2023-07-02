2814e702c9ea7f4adf313c10cbc994010b8a4788
Amr alaa ali <92322929+Amr8tom@users.noreply.github.com>
2023-07-02 12:13:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -101,26 +101,29 @@ app = Flask(__name__)
 
 @app.route('/',methods=["POST","GET"])
 def index():
-    my_model= load_model("model87.h5")
-    image_file = request.files['image']
-    image_bytes = image_file.read()
-    image_np = np.frombuffer(image_bytes, np.uint8)
-    image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
-    # Check if the image was loaded successfully
-    if image is None:
-        return 'Failed to load image', 400
-    
-    # Resize the image using cv2.resize()
-    resized_image = cv2.resize(image, (50, 50))
-    normalized_image = resized_image / 255.0
-    input_image = np.expand_dims(normalized_image, axis=0)
-    assert input_image.shape == (1, 50, 50, 3)
-    y = my_model.predict([input_image])
-    y=y.tolist()
-    ind= np.argmax(y)
-    y=classes[ind]
-    return jsonify({'prediction': y})
-    #return "ya raaaaaaab"
+     
+     if request.method=='post':
+        my_model= load_model("model87.h5")
+        image_file = request.files['image']
+        image_bytes = image_file.read()
+        image_np = np.frombuffer(image_bytes, np.uint8)
+        image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+        # Check if the image was loaded successfully
+        if image is None:
+            return 'Failed to load image', 400
+        
+        # Resize the image using cv2.resize()
+        resized_image = cv2.resize(image, (50, 50))
+        normalized_image = resized_image / 255.0
+        input_image = np.expand_dims(normalized_image, axis=0)
+        assert input_image.shape == (1, 50, 50, 3)
+        y = my_model.predict([input_image])
+        y=y.tolist()
+        ind= np.argmax(y)
+        y=classes[ind]
+        return jsonify({'prediction': y})
+     else:
+         return "ya raaaaaaab"
 
 
 if __name__ == '__main__':
