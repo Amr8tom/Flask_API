8c74c42ab257aa5b2539625913b89ff9a9546f5e
Amr alaa ali <92322929+Amr8tom@users.noreply.github.com>
2023-07-03 00:08:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,30 +19,30 @@ def home ():
 classes = ["non-cancer","cancer"]
 # my_model= models.load_model("model87.h5")
 
-@app.route('/pred',methods=["POST","GET"])
-def index():
-    if request.method=='POST':
-        image_file = request.files['image']
-        image_bytes = image_file.read()
-        image_np = np.frombuffer(image_bytes, np.uint8)
-        image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
-        # Check if the image was loaded successfully
-        if image is None:
-            return 'Failed to load image', 400
+# @app.route('/pred',methods=["POST","GET"])
+# def index():
+#     if request.method=='POST':
+#         image_file = request.files['image']
+#         image_bytes = image_file.read()
+#         image_np = np.frombuffer(image_bytes, np.uint8)
+#         image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+#         # Check if the image was loaded successfully
+#         if image is None:
+#             return 'Failed to load image', 400
         
-        # Resize the image using cv2.resize()
-        resized_image = cv2.resize(image, (50, 50))
-        normalized_image = resized_image / 255.0
-        input_image = np.expand_dims(normalized_image, axis=0)
-        assert input_image.shape == (1, 50, 50, 3)
-        # y = my_model.predict([input_image])
-        # y=y.tolist()
-        y=[0.3,0.5]
-        ind= np.argmax(y)
-        y=classes[ind]
-        return jsonify({'prediction':y})
-    else:
-        return jsonify({'prediction': "ya raaaaaaaab"})
+#         # Resize the image using cv2.resize()
+#         resized_image = cv2.resize(image, (50, 50))
+#         normalized_image = resized_image / 255.0
+#         input_image = np.expand_dims(normalized_image, axis=0)
+#         assert input_image.shape == (1, 50, 50, 3)
+#         # y = my_model.predict([input_image])
+#         # y=y.tolist()
+#         y=[0.3,0.5]
+#         ind= np.argmax(y)
+#         y=classes[ind]
+#         return jsonify({'prediction':y})
+#     else:
+#         return jsonify({'prediction': "ya raaaaaaaab"})
 
 
 
