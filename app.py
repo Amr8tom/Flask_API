785a73c6f51b853465cfbb1232cefac19944d6db
Amr alaa ali <92322929+Amr8tom@users.noreply.github.com>
2023-07-02 13:06:53 +0300

    sd

--- app.py
+++ app.py
@@ -16,28 +16,31 @@ app = Flask(__name__)
 
 @app.route('/',methods=["POST","GET"])
 def index():
-     classes = ["non-cancer","cancer"]
+     
+        
+        
+    #  classes = ["non-cancer","cancer"]
     #  my_model= load_model("model87.h5")
      if request.method=='POST':
-        image_file = request.files['image']
-        image_bytes = image_file.read()
-        image_np = np.frombuffer(image_bytes, np.uint8)
-        image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
-        # Check if the image was loaded successfully
-        if image is None:
-            return 'Failed to load image', 400
+    #     image_file = request.files['image']
+    #     image_bytes = image_file.read()
+    #     image_np = np.frombuffer(image_bytes, np.uint8)
+    #     image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+    #     # Check if the image was loaded successfully
+    #     if image is None:
+    #         return 'Failed to load image', 400
         
-        # Resize the image using cv2.resize()
-        resized_image = cv2.resize(image, (50, 50))
-        normalized_image = resized_image / 255.0
-        input_image = np.expand_dims(normalized_image, axis=0)
-        assert input_image.shape == (1, 50, 50, 3)
-        # y = my_model.predict([input_image])
-        y=[0.4,3.5]
+    #     # Resize the image using cv2.resize()
+    #     resized_image = cv2.resize(image, (50, 50))
+    #     normalized_image = resized_image / 255.0
+    #     input_image = np.expand_dims(normalized_image, axis=0)
+    #     assert input_image.shape == (1, 50, 50, 3)
+    #     # y = my_model.predict([input_image])
+        # y=[0.4,3.5]
         #y=y.tolist()
-        ind= np.argmax(y)
-        y=classes[ind]
-        return jsonify({'prediction': y})
+        # ind= np.argmax(y)
+        # y=classes[ind]
+        return jsonify({'prediction': "cancer"})
      else:
          return "ya raaaaaaab"
 
